d3a70fdbd9792d0d89f931737fcbb5ab5535af60
Justin Latimer <justinlatimer@gmail.com>
2023-01-26 17:34:52 +1300

    SPI mode.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use embedded_hal::spi::{self, Mode};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub const MODE: Mode = spi::MODE_0;
