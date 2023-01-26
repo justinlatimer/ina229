9d3a21ed6472be40ee44ca6b4db707bfdf6139e5
Justin Latimer <justinlatimer@gmail.com>
2023-01-26 21:42:34 +1300

    Start implementing the driver.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,30 @@
 #![no_std]
 
-use embedded_hal::spi::{self, Mode};
+use core::convert::Infallible;
 
-pub const MODE: Mode = spi::MODE_0;
+use embedded_hal::{
+    blocking::spi,
+    digital::v2::OutputPin,
+    spi::{Mode, MODE_1},
+};
+
+pub const MODE: Mode = MODE_1;
+
+pub struct INA229<SPI, NCS> {
+    spi: SPI,
+    ncs: NCS,
+}
+
+impl<SPI, NCS, E> INA229<SPI, NCS>
+where
+    SPI: spi::Write<u8, Error = E> + spi::Transfer<u8, Error = E>,
+    NCS: OutputPin<Error = Infallible>,
+{
+    pub fn new(spi: SPI, ncs: NCS) -> Self {
+        INA229 { spi, ncs }
+    }
+
+    pub fn release(self) -> (SPI, NCS) {
+        (self.spi, self.ncs)
+    }
+}
